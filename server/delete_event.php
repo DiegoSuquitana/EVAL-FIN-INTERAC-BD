5281e23f9434f8c795a863ccf16bc7173beaaaa0
DIEGO SUQUITANA <sistemasdxsa@hotmail.com>
2020-09-19 20:55:56 -0500

    Update delete_event.php

--- server/delete_event.php
+++ server/delete_event.php
@@ -1,5 +1,31 @@
 <?php
 
+include('./conector.php');
 
+session_start();
+
+if (isset($_SESSION['username'])) {
+
+	$data['id'] = $_POST['id'];
+	$query = 'id = '.$data['id'];
+
+	$response['query'] = $query; 
+
+  $con = new ConectorBD('localhost', 't_delete_event', '12345');
+  if ($con->initConexion('agenda_db')=='OK') {
+  	if ($con->eliminarRegistro('evento', $query )) { 
+        $response['msg']= 'OK';
+    }else {
+        $response['msg']= 'No se pudo realizar el borrado de los datos';
+    }
+  }  
+}
+else {
+ $response['msg'] = "No se ha iniciado una sesión";
+}
+
+
+  echo json_encode($response);
+  
 
  ?>
