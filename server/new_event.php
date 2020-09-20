19e1472179ef001669e09b5d7b85dc71d0a3c148
DIEGO SUQUITANA <sistemasdxsa@hotmail.com>
2020-09-19 20:31:01 -0500

    Update new_event.php

--- server/new_event.php
+++ server/new_event.php
@@ -1,5 +1,44 @@
 <?php
   
+  require('./conector.php');
 
+  session_start();
+
+  if (isset($_SESSION['username'])) {
+    $con = new ConectorBD('localhost', 'root', '12345');
+    if ($con->initConexion('agenda_db')=='OK') {
+      $data['titulo'] = "'".$_POST['titulo']."'";
+      $data['fechaInicio'] = "'".$_POST['start_date']."'";
+     
+      if($_POST["allDay"]=="true"){    
+        $allDay  = 1; 
+        $data['fechaFinalizacion'] = "'".$_POST['start_date']."'";    
+        $data['diaCompleto'] = "'".$allDay."'";
+        
+      }else{
+        $allDay = 0;
+        $data['horaInicio'] = "'".$_POST['start_hour']."'";
+        $data['fechaFinalizacion'] = "'".$_POST['end_date']."'";    
+        $data['horaFinalizacion'] = "'".$_POST['end_hour']."'";
+        $data['diaCompleto'] = "'".$allDay."'";
+        
+      }
+      
+      $data['fk_usuario'] = "'".$_SESSION['id']."'";      
+      
+      if ($con->insertData('evento', $data)) {
+        $response['msg']= 'OK';
+      }else {
+        $response['msg']= 'No se pudo realizar la inserción de los datos';
+      }
+    }else {
+      $response['msg']= 'No se pudo conectar a la base de datos';
+    }
+  }else {
+    $response['msg']= 'No se ha iniciado una sesión';
+  }
+
+  echo json_encode($data); //validar los datos que estoy enviado
+  //echo json_encode($response);
 
  ?>
